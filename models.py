6fa0e21847b693db2bbd834e2e8426a4600d79a8
Henry Mao <admin@calclavia.com>
2017-01-14 23:53:20 -0800

    Implemented MIDI output

--- models.py
+++ models.py
@@ -4,6 +4,7 @@ from keras.layers.recurrent import LSTM
 from music import NUM_CLASSES
 
 def rnn_model():
+    # TODO: Use LSTM
     note_input = Input(shape=(NUM_CLASSES,), name='note_input')
     x = note_input
     x = Dense(512, name='h0')(x)
