3e99dfdc39412f3aef76405ab34f5e070bbbade8
Henry Mao <admin@calclavia.com>
2017-01-17 23:17:52 -0800

    Added more classical data

--- midi_experiment.py
+++ midi_experiment.py
@@ -1,17 +1,17 @@
 import numpy as np
+import midi
+import os
 from keras.layers import Dense, Input, Activation, Flatten, Dropout, merge
 from keras.layers.recurrent import GRU
 from keras.layers.convolutional import Convolution1D
 from keras.models import Model
 from util import *
 from midi_util import *
-import midi
-import os
 
 time_steps = 8
 model_save_file = 'out/model.h5'
 
-compositions = load_midi('data/edm')
+compositions = load_midi('data/classical_c')
 
 data_set, beat_set, label_set = [], [], []
 
@@ -19,7 +19,7 @@ for c in compositions:
     x, y = create_dataset(c, time_steps)
     data_set += x
     label_set += y
-    beat_set += create_dataset(create_beat_data(c), time_steps)[0]
+    beat_set += create_dataset(create_beat_data(c, BEATS_PER_BAR), time_steps)[0]
 
 data_set = np.array(data_set)
 label_set = np.array(label_set)
@@ -33,14 +33,14 @@ x = note_input
 
 x = Dropout(0.2)(x)
 # Conv layer
-for i in range(1):
+for i in range(0):
     x = Convolution1D(64, 8, border_mode='same')(x)
     x = Activation('relu')(x)
     x = Dropout(0.5)(x)
 
 x = merge([x, beat_input], mode='concat')
 
-for i in range(3):
+for i in range(2):
     x = GRU(256, return_sequences=True, name='lstm' + str(i))(x)
     x = Activation('relu')(x)
     x = Dropout(0.5)(x)
@@ -62,6 +62,6 @@ model = Model([note_input, beat_input], x)
 model.compile(optimizer='adam', loss='binary_crossentropy',
               metrics=['accuracy'])
 
-model.fit([data_set, beat_set], label_set, nb_epoch=400)
+model.fit([data_set, beat_set], label_set, nb_epoch=20)
 
 model.save(model_save_file)
