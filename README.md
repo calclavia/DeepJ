7a4dbc66af71d98584f799c28d0393efb638b8b2
Henry Mao <admin@calclavia.com>
2017-01-21 12:59:34 -0800

    Adapted some Magenta pipelines

---
+++ README.md
@@ -0,0 +1,10 @@
+# Music Composition using Reinforcement Learning
+
+
+## Requirements
+- Python 3.5
+
+Custom versions of:
+
+- MIDO (https://github.com/calclavia/mido/tree/patch-1)
+- Latest version of PrettyMIDI (https://github.com/craffel/pretty-midi)
--- example_preprocess.py
+++ example_preprocess.py
@@ -1,4 +1,6 @@
 from preprocess import midi_io
+from preprocess import melodies_lib
 
-# Test data loading
-pb = midi_io.midi_to_sequence_proto('data/classical_c/mz_330_1.mid')
+# Test midi load and save
+seq_pb = midi_io.midi_to_sequence_proto('data/edm/edm_c/rc_3.mid')
+midi_io.sequence_proto_to_midi(seq_pb).write('out/test_pb.mid')
