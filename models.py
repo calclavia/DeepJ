4d79341c3670dd73c7e5d3ee62fc85badb00c2bc
Henry Mao <admin@calclavia.com>
2017-01-23 00:27:23 -0800

    Some fixes to supervised train

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 import numpy as np
-from keras.layers import Dense, Input, merge, Activation, Flatten
+from keras.layers import Dense, Input, merge, Activation, Dropout, Flatten
 from keras.models import Model
+from keras.layers.convolutional import Convolution1D
 from keras.layers.recurrent import GRU
 from util import one_hot
 from music import NUM_CLASSES, NOTES_PER_BAR
@@ -30,3 +31,42 @@ def note_model(timesteps):
 def note_preprocess(env, x):
     note, beat = x
     return (one_hot(note, NUM_CLASSES), one_hot(beat, NOTES_PER_BAR))
+
+def supervised_model(time_steps):
+    # Multi-hot vector of each note
+    note_input = Input(shape=(time_steps, NUM_CLASSES), name='note_input')
+    beat_input = Input(shape=(time_steps, NOTES_PER_BAR), name='beat_input')
+
+    x = note_input
+
+    x = Dropout(0.2)(x)
+    # Conv layer
+    for i in range(0):
+        x = Convolution1D(64, 8, border_mode='same')(x)
+        x = Activation('relu')(x)
+        x = Dropout(0.5)(x)
+
+    x = merge([x, beat_input], mode='concat')
+
+    for i in range(2):
+        x = GRU(256, return_sequences=True, name='lstm' + str(i))(x)
+        x = Activation('relu')(x)
+        x = Dropout(0.5)(x)
+
+    x = GRU(256, name='lstm_last')(x)
+    x = Activation('relu')(x)
+    x = Dropout(0.5)(x)
+
+    for i in range(2):
+        x = Dense(256, name='dense' + str(i))(x)
+        x = Activation('relu')(x)
+        x = Dropout(0.5)(x)
+
+    # Multi-label
+    x = Dense(NUM_CLASSES)(x)
+    x = Activation('sigmoid')(x)
+
+    model = Model([note_input, beat_input], x)
+    model.compile(optimizer='adam', loss='binary_crossentropy',
+                  metrics=['accuracy'])
+    return model
--- supervised_train.py
+++ supervised_train.py
@@ -1,66 +1,29 @@
 import numpy as np
 import midi
 import os
-from keras.layers import Dense, Input, Activation, Flatten, Dropout, merge
-from keras.layers.recurrent import GRU
-from keras.layers.convolutional import Convolution1D
-from keras.models import Model
 from util import *
-from midi_util import *
+from models import supervised_model
+from music import NUM_CLASSES, NOTES_PER_BAR
 
 time_steps = 8
 model_save_file = 'out/model.h5'
 
-compositions = load_midi('data/classical_c')
+melodies = load_melodies('data/edm/edm_all')
 
 data_set, beat_set, label_set = [], [], []
 
-for c in compositions:
+for c in melodies:
+    c = [one_hot(x, NUM_CLASSES) for x in c]
     x, y = create_dataset(c, time_steps)
     data_set += x
     label_set += y
-    beat_set += create_dataset(create_beat_data(c, BEATS_PER_BAR), time_steps)[0]
+    beat_set += create_dataset(create_beat_data(c, NOTES_PER_BAR), time_steps)[0]
 
 data_set = np.array(data_set)
 label_set = np.array(label_set)
 beat_set = np.array(beat_set)
 
-# Multi-hot vector of each note
-note_input = Input(shape=(time_steps, NUM_NOTES), name='note_input')
-beat_input = Input(shape=(time_steps, BEATS_PER_BAR), name='beat_input')
-
-x = note_input
-
-x = Dropout(0.2)(x)
-# Conv layer
-for i in range(0):
-    x = Convolution1D(64, 8, border_mode='same')(x)
-    x = Activation('relu')(x)
-    x = Dropout(0.5)(x)
-
-x = merge([x, beat_input], mode='concat')
-
-for i in range(2):
-    x = GRU(256, return_sequences=True, name='lstm' + str(i))(x)
-    x = Activation('relu')(x)
-    x = Dropout(0.5)(x)
-
-x = GRU(256, name='lstm_last')(x)
-x = Activation('relu')(x)
-x = Dropout(0.5)(x)
-
-for i in range(2):
-    x = Dense(128, name='dense' + str(i))(x)
-    x = Activation('relu')(x)
-    x = Dropout(0.5)(x)
-
-# Multi-label
-x = Dense(NUM_NOTES)(x)
-x = Activation('sigmoid')(x)
-
-model = Model([note_input, beat_input], x)
-model.compile(optimizer='adam', loss='binary_crossentropy',
-              metrics=['accuracy'])
+model = supervised_model(time_steps)
 
 model.fit([data_set, beat_set], label_set, nb_epoch=20)
 
