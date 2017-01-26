46bce085b8dd20994f94a5dde3e59299bbed3ad5
Henry Mao <admin@calclavia.com>
2017-01-26 15:04:12 -0800

    Model refactor

--- models.py
+++ models.py
@@ -9,91 +9,62 @@ from util import one_hot
 from music import NUM_CLASSES, NOTES_PER_BAR
 from keras.models import load_model
 
-def note_model(time_steps):
-    # TODO Try dropout again?
-    """
-    note_input = Input(shape=(time_steps, NUM_CLASSES), name='rl/note_input')
-    beat_input = Input(shape=(time_steps, NOTES_PER_BAR), name='rl/beat_input')
-
-    x = merge([note_input, beat_input], mode='concat')
-
-    for i in range(3):
-        x = GRU(128, return_sequences=True, name='rl/lstm' + str(i))(x)
-        x = Activation('relu')(x)
-
-    x = GRU(128, name='rl/lstm_last')(x)
-    x = Activation('relu')(x)
-
-    for i in range(1):
-        x = Dense(256, name='dense' + str(i))(x)
-        x = Activation('relu')(x)
-
-    policy = Dense(NUM_CLASSES, activation='softmax', name='rl/note_policy')(x)
-    value = Dense(1, activation='linear', name='rl/value_output')(x)
-    return Model([note_input, beat_input], [policy, value])
-    """
+def pre_model(time_steps, dropout=True):
     # Multi-hot vector of each note
     note_input = Input(shape=(time_steps, NUM_CLASSES), name='note_input')
     beat_input = Input(shape=(time_steps, NOTES_PER_BAR), name='beat_input')
 
     x = note_input
 
+    if dropout:
+        x = Dropout(0.2)(x)
+
     x = merge([x, beat_input], mode='concat')
 
     for i in range(2):
         x = GRU(256, return_sequences=True, name='lstm' + str(i))(x)
         x = Activation('relu')(x)
+        if dropout:
+            x = Dropout(0.5)(x)
 
     x = GRU(256, name='lstm_last')(x)
     x = Activation('relu')(x)
+    if dropout:
+        x = Dropout(0.5)(x)
 
     for i in range(1):
         x = Dense(256, name='dense' + str(i))(x)
         x = Activation('relu')(x)
+        if dropout:
+            x = Dropout(0.5)(x)
+
+    return [note_input, beat_input], x
+
+def note_model(time_steps):
+    inputs, x = pre_model(time_steps, False)
 
     # Multi-label
     policy = Dense(NUM_CLASSES, name='policy', activation='softmax')(x)
     value = Dense(1, name='value', activation='linear')(x)
 
-    model = Model([note_input, beat_input], [policy, value])
-    # model.load_weights('data/supervised.h5', by_name=True)
+    model = Model(inputs, [policy, value])
+    model.load_weights('data/supervised.h5', by_name=True)
     # Create value output
     return model
+
+
+def supervised_model(time_steps):
+    inputs, x = pre_model(time_steps)
+
+    # Multi-label
+    x = Dense(NUM_CLASSES, name='policy', activation='softmax')(x)
+
+    model = Model(inputs, x)
+    model.compile(optimizer='adam', loss='categorical_crossentropy',
+                  metrics=['accuracy'])
+    return model
+
 
 def note_preprocess(env, x):
     note, beat = x
     return (one_hot(note, NUM_CLASSES), one_hot(beat, NOTES_PER_BAR))
-
-def supervised_model(time_steps):
-    # Multi-hot vector of each note
-    note_input = Input(shape=(time_steps, NUM_CLASSES), name='note_input')
-    beat_input = Input(shape=(time_steps, NOTES_PER_BAR), name='beat_input')
-
-    x = note_input
-
-    x = Dropout(0.2)(x)
-
-    x = merge([x, beat_input], mode='concat')
-
-    for i in range(2):
-        x = GRU(256, return_sequences=True, name='lstm' + str(i))(x)
-        x = Activation('relu')(x)
-        x = Dropout(0.5)(x)
-
-    x = GRU(256, name='lstm_last')(x)
-    x = Activation('relu')(x)
-    x = Dropout(0.5)(x)
-
-    for i in range(1):
-        x = Dense(256, name='dense' + str(i))(x)
-        x = Activation('relu')(x)
-        x = Dropout(0.5)(x)
-
-    # Multi-label
-    x = Dense(NUM_CLASSES, name='policy')(x)
-    x = Activation('softmax')(x)
-
-    model = Model([note_input, beat_input], x)
-    model.compile(optimizer='adam', loss='categorical_crossentropy',
-                  metrics=['accuracy'])
-    return model
