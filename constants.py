d8a4f1577a1a517a693db16be50c785aade9f9d2
Henry Mao <admin@calclavia.com>
2017-03-09 09:57:00 -0800

    Fixed generation

--- constants.py
+++ constants.py
@@ -1,7 +1,7 @@
 # Define the musical styles
-styles = ['data/classical/bach']
-#styles = ['data/classical/mozart', 'data/classical/beethoven', 'data/classical/bach']
+# styles = ['data/classical/bach']
+styles = ['data/classical/mozart', 'data/classical/beethoven', 'data/classical/bach']
 #styles = ['data/edm', 'data/southern_rock', 'data/hard_rock']
 # styles = ['data/edm']
 NUM_STYLES = len(styles)
-BATCH_SIZE = 1
+BATCH_SIZE = 32
--- exp_tf.py
+++ exp_tf.py
@@ -4,13 +4,14 @@ import argparse
 from sklearn import metrics
 from tqdm import tqdm
 
-from dataset import process_stateful, load_music_styles
+from dataset import process_stateful, load_music_styles, get_all_files, compute_beat, compute_completion
 from music import *
 from midi_util import *
-from constants import NUM_STYLES
+from util import chunk
+from constants import NUM_STYLES, styles
 from keras.layers.recurrent import GRU
 
-NUM_NOTES = MAX_NOTE - MIN_NOTE + 2
+NUM_NOTES = MAX_NOTE - MIN_NOTE
 BATCH_SIZE = 64
 TIME_STEPS = 32
 model_file = 'out/saves/model'
@@ -45,6 +46,9 @@ class Model:
         # Target note to predict
         note_target = tf.placeholder(tf.float32, [batch_size, time_steps, NUM_NOTES], name='target_in')
 
+        # Context to help generation
+        contexts = tf.concat([beat_in, progress_in], 2)
+
         """
         Note invariant block
         """
@@ -53,7 +57,7 @@ class Model:
 
         for i in range(NUM_NOTES):
             with tf.variable_scope('rnn1', reuse=i > 0):
-                inv_input = tf.concat([note_in[:, :, i:i+1], beat_in, progress_in, style_in], 2)
+                inv_input = tf.concat([note_in[:, :, i:i+1], contexts], 2)
                 rnn_note_outs.append(rnn(128, inv_input))
 
         # Output of RNN for every octave
@@ -61,7 +65,7 @@ class Model:
 
         for i in range(NUM_OCTAVES):
             with tf.variable_scope('rnn2', reuse=i > 0):
-                inv_input = tf.concat(rnn_note_outs[i:i+OCTAVE], 2)
+                inv_input = tf.concat(rnn_note_outs[i:i+OCTAVE] + [contexts], 2)
                 rnn_octave_outs.append(rnn(512, inv_input))
 
         out = tf.concat(rnn_octave_outs, 2)
@@ -100,8 +104,7 @@ class Model:
         self.saver = tf.train.Saver()
 
     def train(self, sess, train_seqs, num_epochs=100, verbose=True):
-        total_steps = 0
-        patience = 3
+        patience = 10
         no_improvement = 0
         best_fscore = 0
 
@@ -111,23 +114,25 @@ class Model:
             f1_score = 0
             step = 0
 
+            # TODO: Shuffle sequence orders.
             # Bar
             t = tqdm(train_seqs)
             t.set_description('{}/{}'.format(epoch + 1, num_epochs))
 
+            # Train every single sequence
             for seq in t:
                 # Reset state
                 states = [None for _ in self.init_states]
-                inputs, targets = seq
 
-                for X, Y in tqdm(list(zip(inputs, targets))):
+                for note_in, beat_in, progress_in, label in tqdm(seq):
+                    # TODO: Dataset is bugged.
                     # Build feed-dict
                     feed_dict = {
-                        self.note_in: X[0],
-                        self.beat_in: X[1],
-                        self.progress_in: X[2],
-                        self.style_in: X[3],
-                        self.note_target: Y
+                        self.note_in: note_in,
+                        self.beat_in: beat_in,
+                        self.progress_in: progress_in,
+                        # self.style_in: X[3],
+                        self.note_target: label
                     }
 
                     for tf_s, s in zip(self.init_states, states):
@@ -146,16 +151,12 @@ class Model:
                     step += 1
                     # Compute F-1 score of all timesteps and batches
                     # For every single sample in the batch
-                    f1_score += np.mean([metrics.f1_score(y, p, average='weighted') for y, p in zip(Y, pred)])
+                    f1_score += np.mean([metrics.f1_score(y, p, average='weighted') for y, p in zip(label, pred)])
                     t.set_postfix(loss=training_loss / step, f1=f1_score / step)
-
-                    if total_steps % 1000 == 0:
-                        # print('Saving at epoch {}'.format(epoch))
-                        self.saver.save(sess, model_file)
-                    total_steps += 1
 
             # Early stopping
             if f1_score > best_fscore:
+                self.saver.save(sess, model_file)
                 best_fscore = f1_score
                 no_improvement = 0
             else:
@@ -168,16 +169,24 @@ class Model:
         self.saver.save(sess, model_file)
 
     def generate(self, sess, inspiration, length=NOTES_PER_BAR * 16):
+        total_len = length + len(inspiration)
         # Resulting generation
         results = []
         # Reset state
         states = [None for _ in self.init_states]
+
         # Current note
         current_note = np.zeros(NUM_NOTES)
+        current_beat = compute_beat(0, NOTES_PER_BAR)
+        current_progress = compute_completion(0, total_len)
 
-        for i in range(length + len(inspiration)):
+        for i in range(total_len):
             # Build feed dict
-            feed_dict = { self.note_in: [[current_note]] }
+            feed_dict = {
+                self.note_in: [[current_note]],
+                self.beat_in: [[current_beat]],
+                self.progress_in: [[current_progress]]
+            }
 
             for tf_s, s in zip(self.init_states, states):
                 if s is not None:
@@ -204,9 +213,41 @@ def reset_graph():
         sess.close()
     tf.reset_default_graph()
 
+def stagger(data, time_steps):
+    dataX, dataY = [], []
+
+    # First note prediction
+    data = [np.zeros_like(data[0])] + list(data)
+
+    for i in range(len(data) - time_steps - 1):
+        dataX.append(data[i:(i + time_steps)])
+        dataY.append(data[i + 1:(i + time_steps + 1)])
+    return dataX, dataY
+
+def process(sequences):
+    train_seqs = []
+
+    for seq in sequences:
+        train_data, label_data = stagger(seq, TIME_STEPS)
+
+        beat_data = [compute_beat(i, NOTES_PER_BAR) for i in range(len(seq))]
+        beat_data, _ = stagger(beat_data, TIME_STEPS)
+
+        progress_data = [compute_completion(i, len(seq)) for i in range(len(seq))]
+        progress_data, _ = stagger(progress_data, TIME_STEPS)
+
+        # Chunk into batches
+        train_data = chunk(train_data, BATCH_SIZE)
+        beat_data = chunk(beat_data, BATCH_SIZE)
+        progress_data = chunk(progress_data, BATCH_SIZE)
+        label_data = chunk(label_data, BATCH_SIZE)
+        train_seqs.append(list(zip(train_data, beat_data, progress_data, label_data)))
+    return train_seqs
+
 def main():
     parser = argparse.ArgumentParser(description='Generates music.')
     parser.add_argument('--train', default=False, action='store_true', help='Train model?')
+    parser.add_argument('--load', default=False, action='store_true', help='Load model?')
     args = parser.parse_args()
 
     print('Preparing training data')
@@ -215,14 +256,20 @@ def main():
     # TODO: Cirriculum training. Increasing complexity. Increasing timestep details?
     # TODO: Random transpoe?
     # TODO: Random slices of subsequence?
-    sequences = process_stateful(load_music_styles(), TIME_STEPS, batch_size=BATCH_SIZE)
+    sequences = [load_midi(f) for f in get_all_files(styles)]
+    sequences = [np.minimum(np.ceil(m[:, MIN_NOTE:MAX_NOTE]), 1) for m in sequences]
+    train_seqs = process(sequences)
 
     if args.train:
         with tf.Session() as sess:
             print('Training...')
             train_model = Model()
             sess.run(tf.global_variables_initializer())
-            train_model.train(sess, sequences, 100)
+            if args.load:
+                train_model.saver.restore(sess, model_file)
+            else:
+                sess.run(tf.global_variables_initializer())
+            train_model.train(sess, train_seqs, 1000)
 
     reset_graph()
 
