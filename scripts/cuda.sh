4db61ec54f6c426fbc997834a61dec522a988284
Henry Mao <admin@calclavia.com>
2017-03-16 11:38:00 -0700

    Added some Python midi install scripts

--- scripts/cuda.sh
+++ scripts/cuda.sh
@@ -1,10 +1,14 @@
 # Tensorflow setup on Linux instance
 # http://www.nvidia.com/object/gpu-accelerated-applications-tensorflow-installation.html
+# https://www.tensorflow.org/install/install_linux
 
 # Download CUDA
 wget -O cuda.deb https://developer.nvidia.com/compute/cuda/8.0/Prod2/local_installers/cuda-repo-ubuntu1404-8-0-local-ga2_8.0.61-1_amd64-deb
 
 # Ensure same name is used
-sudo dpkg -i cuda.deb
-sudo apt-get update
-sudo apt-get install cuda
+yes | sudo dpkg -i cuda.deb
+yes | sudo apt-get update
+yes | sudo apt-get install cuda
+
+# Install libcupti-dev library
+yes | sudo apt-get install libcupti-dev
--- scripts/python.sh
+++ scripts/python.sh
@@ -1,5 +1,11 @@
 # Python files
-sudo apt-get install python3 python3-pip python3-dev
+yes | sudo apt-get install python3 python3-pip python3-dev
 
 # For Python midi
-sudo apt-get install libasound2-dev python3-augeas swig
+yes | sudo apt-get install libasound2-dev python3-augeas swig
+
+# Install Python MIDI
+git clone https://github.com/vishnubob/python-midi/
+cd python-midi
+sudo python3 setup.py install
+cd ../
