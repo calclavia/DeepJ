595f25d16b00e03ce78b1aaf31cd2425404c06a2
Henry Mao <admin@calclavia.com>
2017-03-16 12:27:21 -0700

    Adjust parameters

--- constants.py
+++ constants.py
@@ -8,12 +8,12 @@ NUM_STYLES = len(styles)
 
 # Training parameters
 BATCH_SIZE = 128
-TIME_STEPS = 16
+TIME_STEPS = 8
 
 # Hyper Parameters
 STYLE_UNITS = 32
 TIME_AXIS_UNITS = [256, 256]
-NOTE_AXIS_UNITS = [128, 128, 256, 256, 512, 512]
+NOTE_AXIS_UNITS = [128, 128, 128, 128, 128, 128]
 
 # Move file save location
 model_file = 'out/saves/model'
