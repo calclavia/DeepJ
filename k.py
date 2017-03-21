3d22a4705267440b1c06f4122e7cabce8a2a8a26
Henry Mao <admin@calclavia.com>
2017-03-21 11:27:39 -0700

    Added temperature

--- k.py
+++ k.py
@@ -94,13 +94,14 @@ def main():
     else:
         write_file(SAMPLES_DIR + '/result.mid', generate(model))
 
-def build_or_load():
+def build_or_load(allow_load=True):
     model = build_model()
-    try:
-        model.load_weights('out/model.h5')
-        print('Loaded model from file.')
-    except:
-        print('Unable to load model from file.')
+    if allow_load:
+        try:
+            model.load_weights('out/model.h5')
+            print('Loaded model from file.')
+        except:
+            print('Unable to load model from file.')
     model.summary()
     return model
 
@@ -124,12 +125,13 @@ def train(model, gen):
 
     model.fit(train_data, train_labels, epochs=1000, callbacks=cbs)
 
-def generate(model):
+def generate(model, default_temp=1):
     print('Generating')
     notes_memory = deque([np.zeros(NUM_NOTES) for _ in range(SEQUENCE_LENGTH)], maxlen=SEQUENCE_LENGTH)
     beat_memory = deque([np.zeros(NOTES_PER_BAR) for _ in range(SEQUENCE_LENGTH)], maxlen=SEQUENCE_LENGTH)
 
     results = []
+    temperature = default_temp
 
     for t in tqdm(range(NOTES_PER_BAR * 8)):
 
@@ -140,9 +142,23 @@ def generate(model):
         for n in range(NUM_NOTES):
             predictions = model.predict([np.array([notes_memory]), np.array([list(notes_memory)[1:] + [next_note]]), np.array([beat_memory])])
             # We only care about the last time step
-            prob = predictions[0][-1]
+            prob_dist = predictions[0][-1]
+
+            # Apply temperature
+            if temperature != 1:
+                # Inverse sigmoid
+                x = -np.log(1 / np.array(prob_dist) - 1)
+                # Apply temperature to sigmoid function
+                prob_dist = 1 / (1 + np.exp(-x / temperature))
+
             # Flip on randomly
-            next_note[n] = 1 if np.random.random() <= prob[n] else 0
+            next_note[n] = 1 if np.random.random() <= prob_dist[n] else 0
+
+        # Increase temperature while silent.
+        if np.count_nonzero(next_note) == 0:
+            temperature += 0.05
+        else:
+            temperature = default_temp
 
         notes_memory.append(next_note)
         # Consistent with dataset representation
