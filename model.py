092772347d7f869cb6389f46ec962c6b3e911dab
Henry Mao <admin@calclavia.com>
2017-05-15 11:08:38 -0700

    Revert "added sigmoid to volume" (#43)
    This reverts commit 9d3a3c3121a93693d3057d90e688f11102b92f07.

--- model.py
+++ model.py
@@ -16,8 +16,8 @@ def primary_loss(y_true, y_pred):
     played = y_true[:, :, :, 0]
     bce_note = losses.binary_crossentropy(y_true[:, :, :, 0], y_pred[:, :, :, 0])
     bce_replay = losses.binary_crossentropy(y_true[:, :, :, 1], tf.multiply(played, y_pred[:, :, :, 1]) + tf.multiply(1 - played, y_true[:, :, :, 1]))
-    bce_volume = losses.binary_crossentropy(y_true[:, :, :, 2], tf.multiply(played, y_pred[:, :, :, 2]) + tf.multiply(1 - played, y_true[:, :, :, 2]))
-    return bce_note + bce_replay + bce_volume
+    mse = losses.mean_squared_error(y_true[:, :, :, 2], tf.multiply(played, y_pred[:, :, :, 2]) + tf.multiply(1 - played, y_true[:, :, :, 2]))
+    return bce_note + bce_replay + mse
 
 def style_loss(y_true, y_pred):
     return 0.5 * losses.categorical_crossentropy(y_true, y_pred)
@@ -95,7 +95,8 @@ def note_axis(dropout):
     dense_layer_cache = {}
     lstm_layer_cache = {}
     note_dense = Dense(2, activation='sigmoid', name='note_dense')
-    volume_dense = Dense(1, activation='sigmoid', name='volume_dense')
+    volume_dense = Dense(1, name='volume_dense')
+    # final_dense = Concatenate()([note_dense, volume_dense])
 
     def f(x, chosen, style):
         time_steps = int(x.get_shape()[1])
