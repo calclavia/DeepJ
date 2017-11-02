93524b4f4e54486e97dc639874a0904d4604b93d
Henry Mao <admin@calclavia.com>
2017-11-01 17:49:04 -0700

    Add style after each layer

--- model.py
+++ model.py
@@ -16,22 +16,21 @@ class DeepJ(nn.Module):
         self.style_units = style_units
 
         # RNN
-        # self.rnns = [nn.LSTMCell(NUM_ACTIONS if i == 0 else self.num_units, self.num_units) for i in range(num_layers)]
-        self.rnn = nn.LSTM(NUM_ACTIONS + self.style_units, num_units, num_layers, batch_first=True)
+        self.rnns = [nn.LSTM(NUM_ACTIONS if i == 0 else self.num_units, self.num_units, batch_first=True) for i in range(num_layers)]
 
         self.output = nn.Linear(self.num_units, NUM_ACTIONS)
         self.softmax = nn.Softmax()
 
-        # for i, rnn in enumerate(self.rnns):
-            # self.add_module('rnn_' + str(i), rnn)
+        for i, rnn in enumerate(self.rnns):
+            self.add_module('rnn_' + str(i), rnn)
 
         # Style
         self.style_linear = nn.Linear(NUM_STYLES, self.style_units)
-        # self.style_layers = [nn.Linear(self.style_units, NUM_ACTIONS if i == 0 else self.num_units) for i in range(num_layers)]
-        # self.tanh = nn.Tanh()
+        self.style_layers = [nn.Linear(self.style_units, self.num_units) for i in range(num_layers)]
+        self.tanh = nn.Tanh()
 
-        # for i, layer in enumerate(self.style_layers):
-            # self.add_module('style_layers_' + str(i), layer)
+        for i, layer in enumerate(self.style_layers):
+            self.add_module('style_layers_' + str(i), layer)
 
     def forward(self, x, style, states=None):
         batch_size = x.size(0)
@@ -42,28 +41,23 @@ class DeepJ(nn.Module):
         style = self.style_linear(style)
 
         ## Process RNN ##
-        # if states is None:
-            # states = [[var(torch.zeros(batch_size, self.num_units)) for _ in range(2)] for _ in range(self.num_layers)]
+        if states is None:
+            states = [None for _ in range(self.num_layers)]
 
-        """
         for l, rnn in enumerate(self.rnns):
-            # prev_x = x
+            prev_x = x
+
+            x, states[l] = rnn(x, states[l])
 
             # Style integration
-            # style_activation = self.tanh(self.style_layers[l](style))
-            # x = x + style_activation
-
-            x, state = rnn(x, states[l])
-            states[l] = (x, state)
+            style_activation = self.tanh(self.style_layers[l](style))
+            style_seq = style_activation.unsqueeze(1)
+            style_seq = style_seq.expand(batch_size, seq_len, self.num_units)
+            x = x + style_seq
 
             # Residual connection
-            # if l != 0:
-                # x = x + prev_x
-        """
-        style_seq = style.unsqueeze(1)
-        style_seq = style_seq.expand(batch_size, seq_len, self.style_units)
-        x = torch.cat((x, style_seq), 2)
-        x, states = self.rnn(x, states)
+            if l != 0:
+                x = x + prev_x
 
         x = self.output(x)
         return x, states
